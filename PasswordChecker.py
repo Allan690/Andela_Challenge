fc68b630a6cbd99f51fab38ebd2a2344d9218822
Allan690 <allannelly690@gmail.com>
2018-09-18 22:00:20 +0300

    Updated Code

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -1,15 +1,15 @@
-def PasswordValidator():
-   passwords = input("Enter your string of passwords: ").split(',')
-   special_str = "$#@"
-   accepted = []
-   for password in passwords:
-       lower = 0
-       upper = 0
-       digits = 0
-       special = 0
-       if(len(password)<6 or len(password)>12):
-         return 'Password is invalid'
-       for char in password:
+# from unittest import TestCase
+
+def handle_user_input():
+  passwords = input("Enter your string of passwords: ").split(',')
+  return passwords
+
+def validate_password(passwords):
+  special_str = "$#@"
+  accepted = []
+  for password in passwords:
+    lower, upper, digits, special = 0, 0, 0, 0
+    for char in password:
             if char.isdigit():
                 digits += 1
             elif char.islower():
@@ -18,11 +18,23 @@ def PasswordValidator():
                 upper += 1
             elif special_str.find(char) != -1:
                 special += 1
-       if lower >= 1 and upper >= 1 and digits >= 1 and special >= 1 and len(password) in range(6,13):
-            accepted.append(password)
-   print (",".join(accepted))
+    if lower and upper and digits and special and len(password) in range(6,13):
+      accepted.append(password)
 
+  return accepted
+
+def main():
+  passwords= handle_user_input()
+  accepted = validate_password(passwords)
+  print (",".join(accepted))
+
+
+# class PasswordTester(unittest.TestCase):
+#   def test_valid_passwords(self):
+    #self.assertEqual(validate_password(["I@n123", "T@st"]), "I@n123, T@st"")
+    # pass
+
+  
 if __name__=='__main__':
-    PasswordValidator()
-            
-
+    # unittest.main()
+    main()
